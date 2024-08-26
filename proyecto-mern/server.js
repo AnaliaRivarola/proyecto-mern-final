d25b8278e5feaab761bf20fc43bec36853759dd3
AnaliaRivarola <80989474+AnaliaRivarola@users.noreply.github.com>
2024-08-25 22:07:55 -0400

    multer

--- proyecto-mern/server.js
+++ proyecto-mern/server.js
@@ -1,8 +1,28 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
+const path = require('path'); // Opcional, si necesitas trabajar con rutas de archivo
+const fs = require('fs');
+
 const app = express();
 const port = 8000;
+
+// Configura multer
+const upload = multer({ dest: 'uploads/' });
+
+// Define las rutas
+app.post('/api/pets', upload.single('imagenMascota'), (req, res) => {
+    console.log(req.file); // Verifica la información del archivo recibido
+    saveImage(req.file);
+    res.send('Termina');
+});
+
+function saveImage(file) {
+    const newPath = `./uploads/${file.originalname}`;
+    // Renombra el archivo a la nueva ruta
+    fs.renameSync(file.path, newPath);
+    return newPath;
+}
 
 require('./config/baseDatos');
 
@@ -10,7 +30,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Incluye las rutas desde otro archivo
 require('./routes/rutaMascota')(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
-
